0ff7cc2d6cfa2f90fc3c55419b06a54a563c9611
carbersor <carbersor@alum.us.es>
2024-03-23 19:50:27 +0100

    feat: Add horizontal proyection method

--- src/stuff_region_segmentation.py
+++ src/stuff_region_segmentation.py
@@ -15,12 +15,23 @@ def binary_transform(image):
     Salidas:
         binary_image (array): La imagen transformada binariamente.
     '''
-    _, binary_image = cv2.threshold(image, BINARY_TRANFORM_THRESHOLD, 255, cv2.THRESH_BINARY)
+    _, binary_image = cv2.threshold(
+        image, BINARY_TRANFORM_THRESHOLD, 255, cv2.THRESH_BINARY)
     return binary_image
 
 
 def horizontal_proyection(binary_image):
-    pass
+    '''
+    Realiza una proyección horizontal de una imagen binaria dada para obtener así su histograma de proyección horizontal.
+
+    Entradas:
+        image (array): Una matriz que representa la imagen binaria de entrada.
+
+    Salidas:
+        horizontal_hist (array): Un histograma de proyección horizontal.
+    '''
+    horizontal_hist = np.sum(binary_image, axis=1)
+    return horizontal_hist
 
 
 def region_segmentation(image):
@@ -35,6 +46,6 @@ def test_binary_transform():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 if __name__ == '__main__':
     test_binary_transform()
-    
