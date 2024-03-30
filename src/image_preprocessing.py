ec877fd198203f65ec05f19f36ff074b9a8514b7
tcabgom <tcabgom222@gmail.com>
2024-03-30 21:20:11 +0100

    feat: Added future function templates

---
+++ src/image_preprocessing.py
@@ -0,0 +1,12 @@
+
+
+def staff_line_filtering(image):
+    pass
+
+
+def morphological_processing(image):
+    pass
+
+
+def connected_component_labeling(image):
+    pass
---
+++ src/note_recognition.py
@@ -0,0 +1,16 @@
+
+
+def stem_filtering(image):
+    pass
+
+
+def size_filtering(image):
+    pass
+
+
+def shape_filtering(image):
+    pass
+
+
+def pitch_and_beat_analysis(image):
+    pass
